87901265a8ca3fca37028cc2c99da70d1c8b9a7a
SuavePirate <alexander.dunn@outlook.com>
2017-04-22 18:45:02 -0400

    Part 1 Complete

---
+++ src/SuaveChallenge/SuaveChallenge/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace CoolBreeze
+{
+    public partial class App : Application
+    {
+        public static MainViewModel ViewModel;
+        public static string RegistrationCode = "XBWWBE3161B54E8FF05C2606431B6535";
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CoolBreeze.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ src/SuaveChallenge/SuaveChallenge/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace CoolBreeze
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            if (App.ViewModel == null) App.ViewModel = new MainViewModel();
+
+            this.BindingContext = App.ViewModel;
+
+            if (App.ViewModel.NeedsRefresh) App.ViewModel.RefreshCurrentConditionsAsync();
+
+            if (cityPicker.SelectedIndex < 0) cityPicker.SelectedIndex = 0;
+
+            base.OnAppearing();
+        }
+
+        private void SelectedCityChanged(object sender, EventArgs e)
+        {
+            if (!App.ViewModel.IsBusy)
+            {
+                App.ViewModel.NeedsRefresh = true;
+                App.ViewModel.LocationType = Common.LocationType.City;
+
+                string selectedItem = (sender as Picker).Items[(sender as Picker).SelectedIndex];
+
+                var cityName = selectedItem.Split('(').First().Trim();
+                var countryCode = selectedItem.Split('(').Last().Replace(")", "").Trim();
+
+                App.ViewModel.CityName = cityName;
+                App.ViewModel.CountryCode = countryCode;
+
+                App.ViewModel.RefreshCurrentConditionsAsync();
+            }
+        }
+    }
+}
